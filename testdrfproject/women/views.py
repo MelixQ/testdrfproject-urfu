8ca5d73ae0fe8008a6fda342061da4b0d1db6ef9
MelixQ <melixmilan@gmail.com>
2023-07-10 13:18:09 +0500

    Lecture #5 complete

--- testdrfproject/women/views.py
+++ testdrfproject/women/views.py
@@ -15,13 +15,34 @@ class WomenAPIView(APIView):
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            category_id=request.data['category_id']
-        )
+        serializer.save()
+        return Response({'post': serializer.data})
 
-        return Response({'post': WomenSerializer(post_new).data})
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method PUT is not allowed"})
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exist"})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"post": serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method DELETE is not allowed"})
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exist"})
+
+        instance.delete()
+        return Response({"delete": "Instance deleted"})
 
 # class WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
