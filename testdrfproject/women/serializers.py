5327076e0ed5f463d6bb734addb02c46723373f2
MelixQ <melixmilan@gmail.com>
2023-07-10 12:45:30 +0500

    Lecture #4 complete

--- testdrfproject/women/serializers.py
+++ testdrfproject/women/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'category_id')
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    category_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = WomenModel('Megan Fox', 'American actress')
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
